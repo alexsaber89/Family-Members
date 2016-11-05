9f383b85ab8060f475748eb464b95f5395f57d19
Alex Saber <Alex Saber>
2016-11-05 17:44:04 -0500

    family member skills now print to the DOM, with correct punctuation

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,18 @@
 function displayFamilyMembers(familyMembers) {
   $("#family-members-container").html("");
   familyMembers.forEach(function(member) {
-    let familyMember =`<div class="col-xs-12 family-member-container">`;
-    familyMember+=`<button class="btn btn-default col-xs-1 edit">Edit</button>`;
-    familyMember+=`<button class="btn btn-danger col-xs-1 delete">Delete</button>`;
-    familyMember+=`<p class="col-xs-10">${member.name} (${member.age}, ${member.gender})</p>`;
-    familyMember+='</div>';
+    let skills = "Enjoys ";
+    let familyMember =`<div class="col-xs-12 family-member-container" fbid="${member.id}">`;
+    familyMember +=`<button class="btn btn-danger col-xs-1 delete">Delete</button>`;
+    member.skills.forEach(function(skill, index, array) {
+      if (index !== array.length - 1) {
+        skills += `${skill}, `;
+      } else {
+        skills += `and ${skill}.`;
+      }
+    });
+    familyMember += `<p class="col-xs-11">${member.name} (${member.age}, ${member.gender}).  ${skills}</p>`;
+    familyMember += '</div>';
     $("#family-members-container").append(familyMember);
   });
 }
