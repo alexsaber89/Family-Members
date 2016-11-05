aea7ad5fcf35f22a805538076dae56e85316517d
Alex Saber <Alex Saber>
2016-11-05 09:19:52 -0500

    local api keys are being passed to firebase initialization

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-var $ = require("../lib/node_modules/jquery");
+let getFirebaseCredentials = require("./getFirebaseCredentials");
+
+$(document).ready(function() {
+
+  getFirebaseCredentials().then(function(keys) {
+    let apiKeys = keys;
+    console.log("api keys: ", apiKeys);
+    firebase.initializeApp(apiKeys);
+  });
+
+
+
+});
